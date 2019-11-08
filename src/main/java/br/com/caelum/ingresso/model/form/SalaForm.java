85c88e3841b5c2447dd4817b8dfbf432dadf3da4
alexandre <paulino21@bol.com.br>
2019-11-07 21:09:24 -0300

    adionado preco

--- src/main/java/br/com/caelum/ingresso/model/form/SalaForm.java
+++ src/main/java/br/com/caelum/ingresso/model/form/SalaForm.java
@@ -1,5 +1,6 @@
 package br.com.caelum.ingresso.model.form;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -14,6 +15,7 @@ public class SalaForm {
 
     @NotBlank
     private String nome;
+    private BigDecimal preco ;
 
     private List<Lugar> lugares = new ArrayList<>();
 
@@ -24,6 +26,7 @@ public class SalaForm {
         this.salaId = sala.getId();
         this.nome = sala.getNome();
         this.lugares = new ArrayList<>(sala.getLugares());
+        this.preco = sala.getPreco();
     }
 
     public Integer getSalaId() {
@@ -49,9 +52,18 @@ public class SalaForm {
     public void setLugares(List<Lugar> lugares) {
         this.lugares = lugares;
     }
+    
 
-    public Sala toSala() {
-        Sala sala = new Sala(this.nome);
+    public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public Sala toSala() {
+        Sala sala = new Sala(this.nome,this.preco);
         sala.setId(this.salaId);
         sala.setLugares(new HashSet<>(this.lugares));
         return sala;
